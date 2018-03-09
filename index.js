9d5ebd570edcf56ac4e522680c09d4369f14275e
caillet <jp78920@hotmail.com>
2018-03-09 15:29:29 +0100

    bot complete

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const https = require('https');
 const request = require('request');
 
-
-class Botcarefour {
+module.exports = class Botcarefour {
   constructor (longitude, latitude) {
     this.longitude = longitude;
     this.latitude = latitude;
@@ -13,25 +11,27 @@ class Botcarefour {
    * @return {Error | string} result
    */
   init (callback) {
-    const options = { method: 'GET',
-  url: 'https://api.fr.carrefour.io/v1/openapi/stores',
-  qs: 
-   { longitude: this.longitude,
-     latitude: this.latitude,
-     radius: '10000',
+    const options = {'method': 'GET',
+      'url': 'https://api.fr.carrefour.io/v1/openapi/stores',
+      'qs':
+   {'longitude': this.longitude,
+     'latitude': this.latitude,
+     'radius': '10000'
    },
-  headers: 
-   { accept: 'application/json',
+      'headers':
+   {'accept': 'application/json',
      'x-ibm-client-secret': 'cJ5rG8fO8tV1jI3cO7gJ3qP3pM3pG4rV8mQ0yI5qX0bP0aR5oW',
-     'x-ibm-client-id':  '64d77f04-4ad0-4dc4-a32e-f8cad1426369'
-   } 
- };
-    request(options, function (error, response, body) {
-  if (error) return  console.error('Failed: %s', error.message);
-  //console.log( body);
-  callback(body);
-  });
-   
+     'x-ibm-client-id': '64d77f04-4ad0-4dc4-a32e-f8cad1426369'
+   }
+    };
+
+    request(options, function requeste (error, response, body) {
+      if (error) {
+        return console.error('Failed: %s', error.message);
+      }
+      callback(body);
+      return body;
+    });
   }
   /**
    * Run
@@ -39,6 +39,7 @@ class Botcarefour {
    */
   run () {
     var sync = true;
+
     this.init(result => {
       //console.log(result);
       this.setJson(result);
@@ -51,22 +52,21 @@ class Botcarefour {
   }
 
   setJson (json) {
-    this.json =  JSON.parse(json) ;
+    this.json = JSON.parse(json);
   }
 
   getJson () {
     return this.json;
   }
-  getAdress(i){
+  getAdress (i) {
     return this.json.list[i].address;
   }
-};
-
-  
-
-const test = new Botcarefour(2.3823087999999997 , 48.896551699999996);
-test.run( );
-console.log(test.getAdress(0));
-
+  getLongitude (i) {
+    return this.json.list[i].longitude;
+  }
+  getLatitude (i) {
+    return this.json.list[i].latitude;
+  }
+}
 
 
